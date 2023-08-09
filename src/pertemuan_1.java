13cb1e44d8fcec6c03837a1c04274113ab4bc267
Yovan <yovanwisely@gmail.com>
2023-08-09 14:13:07 +0700

    init

---
+++ src/pertemuan_1.java
@@ -0,0 +1,19 @@
+
+
+public class pertemuan_1 {
+    public static void main(String[] args) {
+
+
+        System.out.println("--- Data Mahasiswa ---");
+        System.out.println("Nama: Yovan Wisely Anstandi");
+        System.out.println("NRP: c14230068");
+        System.out.println("Mata Kuliah: Alrogitma dan Pemrograman");
+        System.out.println("Kelas: B");
+        System.out.println("Tahun: 2023/2024");
+        System.out.println("Semester: Gasal");
+        System.out.println("------------------------");
+
+
+
+    }
+}
